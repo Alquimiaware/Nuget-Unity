fe960fc24787dbdd3df024b94d367865382c9201
Rafael Muñoz <raudo18@gmail.com>
2015-09-30 13:50:39 +0200

    Added List Command args and support for search terms

---
+++ Assets/NuGet-Unity/Editor/ListCommandArgs.cs
@@ -0,0 +1,33 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using UnityEngine;
+    using System.Collections;
+    using System;
+
+    public class ListCommandArgs : CommandArgsBuilder
+    {
+        public ListCommandArgs(Sources sources)
+            : base(sources)
+        { }
+
+        public string SearchTerms { get; internal set; }
+
+        protected override string CommandName
+        {
+            get
+            {
+                return "list";
+            }
+        }
+
+        protected override string GetDirectParams()
+        {
+            return this.SearchTerms ?? string.Empty;
+        }
+
+        protected override string GetMoreOptions()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Assets/NuGet-Unity/Editor/Tests/ListCommandArgsTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Alquimiaware.NuGetUnity.Tests
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ListCommandArgsTests
+    {
+        [Test]
+        public void ToString_ByDefinition_HasListCommandName()
+        {
+            var sources = DefaultSources();
+            var sut = new ListCommandArgs(sources);
+            var args = sut.ToString();
+            Assert.AreEqual("list", args.Split(' ')[0]);
+        }
+
+        [Test]
+        public void ToString_HasSearchTerms_IncludesSearchTerms()
+        {
+            string terms = "FooBar";
+            var sources = DefaultSources();
+            var sut = new ListCommandArgs(sources);
+            sut.SearchTerms = terms;
+
+            var args = sut.ToString();
+
+            // TODO: Check whether several terms are valid
+            // Check whether we need to quote the whole block
+            Assert.AreEqual(terms, args.Split(' ')[1]);
+        }
+
+        private Sources DefaultSources()
+        {
+            var sources = ScriptableObject.CreateInstance<Sources>();
+            sources.AddLocal("R:/MyLocalRepo/");
+            return sources;
+        }
+    }
+}
