4a703e6b31d249bf1018133ee3de4f1b9bb02325
Rafael Muñoz <raudo18@gmail.com>
2015-10-06 16:08:09 +0200

    Added ListCommand Interactor and a common base for all commands

---
+++ Assets/NuGet-Unity/Editor/Interactors/ListCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    public class ListCommand : NuGetCommand
+    {
+        public ListCommand(Sources sources)
+            : base(sources)
+        { }
+
+        public bool ShowPrerelease { get; set; }
+        public bool ShowAllVersions { get; set; }
+
+        public string Execute(string searchTerms)
+        {
+            var args = new ListCommandArgs(this.Sources);
+            args.SearchTerms = searchTerms;
+            args.ShowAllVersions = this.ShowAllVersions;
+            args.ShowPrerelase = this.ShowPrerelease;
+
+            return CallNuGet(args.ToString());
+        }
+    }
+
+}
---
+++ Assets/NuGet-Unity/Editor/Interactors/NuGetCommand.cs
@@ -0,0 +1,44 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using System.Diagnostics;
+    using System.IO;
+    using UnityEngine;
+
+    public class NuGetCommand
+    {
+        public NuGetCommand(Sources sources)
+        {
+            this.Sources = sources;
+        }
+
+        protected Sources Sources { get; private set; }
+
+        /// <summary>
+        /// Calls nuget, blocks until completion and returns output.
+        /// </summary>
+        /// <param name="args">nuget.exe args to be passed</param>
+        /// <returns>The output or error stream.</returns>
+        protected static string CallNuGet(string args)
+        {
+            UnityEngine.Debug.Log(args);
+            var nugetFullPath = Directory.GetFiles(
+                Application.dataPath,
+                "nuget.exe",
+                SearchOption.AllDirectories)[0];
+
+            var startInfo = new ProcessStartInfo(nugetFullPath, args);
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+
+            var process = Process.Start(startInfo);
+            var stdOut = process.StandardOutput.ReadToEnd();
+            var stdError = process.StandardError.ReadToEnd();
+
+            return !string.IsNullOrEmpty(stdOut) ?
+                   stdOut :
+                   stdError;
+        }
+    }
+}
