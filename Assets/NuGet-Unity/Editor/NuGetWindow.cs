a5774f30488522a854307711f4b8e71b82214bb4
Rafael Muñoz <raudo18@gmail.com>
2015-10-06 16:09:27 +0200

    Added menu item to create the NuGet Window

--- Assets/NuGet-Unity/Editor/NuGetWindow.cs
+++ Assets/NuGet-Unity/Editor/NuGetWindow.cs
@@ -7,47 +7,16 @@
 
     public class NuGetWindow : EditorWindow
     {
-        [MenuItem("Run/Foo")]
-        public static void Foo()
+        [MenuItem("Window/NuGet")]
+        private static void Init()
         {
-            Sources sources = GetSources();
-            var listCmdArgs = new ListCommandArgs(sources);
-            var output = CallNuGet(listCmdArgs.ToString());
-            UnityEngine.Debug.Log(output);
+            GetWindow<NuGetWindow>("NuGet").Show();
         }
 
         private static Sources GetSources()
         {
             var sourceAssetPath = FindFirst("t:Sources");
             return AssetDatabase.LoadAssetAtPath<Sources>(sourceAssetPath);
-        }
-
-        /// <summary>
-        /// Calls nuget, blocks until completion and returns output.
-        /// </summary>
-        /// <param name="args">nuget.exe args to be passed</param>
-        /// <returns>The output or error stream.</returns>
-        private static string CallNuGet(string args)
-        {
-            UnityEngine.Debug.Log(args);
-            var nugetFullPath = Directory.GetFiles(
-                Application.dataPath,
-                "nuget.exe",
-                SearchOption.AllDirectories)[0];
-
-            var startInfo = new ProcessStartInfo(nugetFullPath, args);
-            startInfo.RedirectStandardOutput = true;
-            startInfo.RedirectStandardError = true;
-            startInfo.UseShellExecute = false;
-            startInfo.CreateNoWindow = true;
-
-            var process = Process.Start(startInfo);
-            var stdOut = process.StandardOutput.ReadToEnd();
-            var stdError = process.StandardError.ReadToEnd();
-
-            return !string.IsNullOrEmpty(stdOut) ?
-                   stdOut :
-                   stdError;
         }
 
         /// <summary>
