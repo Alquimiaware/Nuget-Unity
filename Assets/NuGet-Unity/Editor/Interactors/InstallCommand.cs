84ef8907b5aef81cf12c8c4e98b4b88c527c6733
Rafael Muñoz <raudo18@gmail.com>
2015-10-07 17:47:35 +0200

    Created InstallCommand interactor.

---
+++ Assets/NuGet-Unity/Editor/Interactors/InstallCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    public class InstallCommand : NuGetCommand
+    {
+        public InstallCommand(Sources sources)
+            : base(sources)
+        { }
+
+        public string OutputDir { get; set; }
+        public bool AllowPrerelease { get; set; }
+
+        public string Execute(string packageName, string version)
+        {
+            var installCmdArgs = new InstallCommandArgs(this.Sources);
+            installCmdArgs.PackageName = packageName;
+            installCmdArgs.Version = version;
+            installCmdArgs.OutputDirectory = OutputDir;
+            installCmdArgs.AllowPrerelease = this.AllowPrerelease;
+
+            return CallNuGet(installCmdArgs.ToString());
+        }
+    }
+}
