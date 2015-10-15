ce9aa9a36b8198e68ddc502ed5085b4dddc75979
Rafael Muñoz <raudo18@gmail.com>
2015-10-16 01:05:18 +0200

    Added basic detection of valid packages sources

--- Assets/NuGet-Unity/Editor/FileSystemPackageProvider.cs
+++ Assets/NuGet-Unity/Editor/FileSystemPackageProvider.cs
@@ -1,17 +1,29 @@
 ﻿namespace Alquimiaware.NuGetUnity
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
 
     public class FileSystemPackageProvider : IPackageProvider
     {
         public List<Package> GetAll(string packagesFolderPath)
         {
+            if (!this.IsPackageSource(packagesFolderPath))
+                throw new ArgumentOutOfRangeException("packagesFolderPath");
+
             return null;
         }
 
         public bool IsPackageSource(string packagesFolderPath)
         {
-            return true;
+            return Directory.Exists(packagesFolderPath)
+                && !IsEmpty(packagesFolderPath);
+        }
+
+        private bool IsEmpty(string folderPath)
+        {
+            var di = new DirectoryInfo(folderPath);
+            return di.GetDirectories().Length == 0;
         }
     }
 }
--- Assets/NuGet-Unity/Editor/Tests/FileSystemPackageProviderTests.cs
+++ Assets/NuGet-Unity/Editor/Tests/FileSystemPackageProviderTests.cs
@@ -4,52 +4,136 @@
     using NUnit.Framework;
     using System.IO;
 
+    [TestFixture, Category("Integration")]
     public class FileSystemPackageProviderTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            Directory.CreateDirectory(SourcePath());
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Directory.Delete(SourcePath(), true);
+        }
+
+        [Test]
+        public void IsPackageSource_FolderNotExists_ReturnsFalse()
+        {
+            var sut = Default();
+            var isValid = sut.IsPackageSource(
+                Path.Combine(SourcePath(), "FooBarNotThere"));
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void IsPackageSource_EmptyFolder_ReturnsFalse()
+        {
+            var sut = Default();
+            var isValid = sut.IsPackageSource(SourcePath());
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void IsPackageSource_SourceHasOnePackage_ReturnsTrue()
+        {
+            CreatePackage("FooBar", "net35", "net20");
+            var sut = Default();
+            Assert.IsTrue(sut.IsPackageSource(SourcePath()));
+        }
+
+        [Test]
+        public void IsPackageSource_SourceHasSeveralPackages_ReturnsTrue()
+        {
+            CreatePackage("Sonic", "net35", "net20");
+            CreatePackage("Ruby", "net35", "net20");
+            CreatePackage("Green", "net35", "net20");
+            var sut = Default();
+            Assert.IsTrue(sut.IsPackageSource(SourcePath()));
+        }
+
+        [Category("Integration")]
         public class ContractTests : IPackageProviderContractTests
         {
-            private string testPath;
-            private string emptySourcePath;
-            private string validSourcePath;
-
             [SetUp]
             public void Setup()
             {
-                this.testPath = Path.Combine(
-                    Application.dataPath,
-                    "../Library/FSContractTests/");
-                this.emptySourcePath = Path.Combine(
-                    this.testPath,
-                    "EmptyPackageSource/");
-                this.validSourcePath = Path.Combine(
-                    this.testPath,
-                    "ValidPackageSource/");
-
-                Directory.CreateDirectory(testPath);
-                Directory.CreateDirectory(emptySourcePath);
-                Directory.CreateDirectory(validSourcePath);
+                Directory.CreateDirectory(SourcePath());
             }
 
             [TearDown]
-            public void TearDown()
+            public void Teardown()
             {
-                Directory.Delete(this.testPath, true);
+                Directory.Delete(SourcePath(), true);
             }
 
             protected override IPackageProvider CreateProvider()
             {
-                return new FileSystemPackageProvider();
+                return Default();
             }
 
             protected override string GetInvalidSourcePath()
             {
-                return emptySourcePath;
+                return SourcePath(); // Empty by default
             }
 
             protected override string GetValidSourcePath()
             {
-                return validSourcePath;
+                CreatePackage("KuKoo", "net35");
+                return SourcePath();
             }
+        }
+
+        private static FileSystemPackageProvider Default()
+        {
+            return new FileSystemPackageProvider();
+        }
+
+        public static string SourcePath()
+        {
+            return Path.Combine(
+                Application.dataPath,
+                "../Library/FSTestsSource/");
+        }
+
+        public static DirectoryInfo CreateEmptyFolder(string name)
+        {
+            var folderPath = Path.Combine(
+                SourcePath(),
+                name);
+
+            return Directory.CreateDirectory(folderPath);
+        }
+
+        public static void CreatePackage(
+            string name,
+            params string[] targetFrameworks)
+        {
+            var pkgRoot = CreateEmptyFolder(name);
+            var lib = pkgRoot.CreateSubdirectory("lib");
+            foreach (var targetName in targetFrameworks)
+                CreateTargetFrameworkSpecific(name, targetName, lib);
+            CreateFile(name + ".nupkg", pkgRoot);
+            CreateFile("[Content_Types].xml", pkgRoot);
+        }
+
+        private static void CreateTargetFrameworkSpecific(
+            string name,
+            string targetName,
+            DirectoryInfo container)
+        {
+            var di = container.CreateSubdirectory(targetName);
+            CreateFile(name + ".dll", di);
+            CreateFile(name + ".xml", di);
+            CreateFile(name + ".pdb", di);
+        }
+
+        private static void CreateFile(string name, DirectoryInfo di)
+        {
+            File.Create(Path.Combine(di.FullName, name))
+                .Dispose();
         }
     }
 }
