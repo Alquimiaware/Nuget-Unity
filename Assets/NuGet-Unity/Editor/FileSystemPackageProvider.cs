b68b5de58d84a64a0b1932c52298d8e4427baf66
Rafael Muñoz <raudo18@gmail.com>
2015-10-15 17:55:31 +0200

    Added Contract tests for FileSystemPackageProvider

---
+++ Assets/NuGet-Unity/Editor/FileSystemPackageProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using System.Collections.Generic;
+
+    public class FileSystemPackageProvider : IPackageProvider
+    {
+        public List<Package> GetAll(string packagesFolderPath)
+        {
+            return null;
+        }
+
+        public bool IsPackageSource(string packagesFolderPath)
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/NuGet-Unity/Editor/Tests/FileSystemPackageProviderTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Alquimiaware.NuGetUnity.Tests
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using System.IO;
+
+    public class FileSystemPackageProviderTests
+    {
+        public class ContractTests : IPackageProviderContractTests
+        {
+            private string testPath;
+            private string emptySourcePath;
+            private string validSourcePath;
+
+            [SetUp]
+            public void Setup()
+            {
+                this.testPath = Path.Combine(
+                    Application.dataPath,
+                    "../Library/FSContractTests/");
+                this.emptySourcePath = Path.Combine(
+                    this.testPath,
+                    "EmptyPackageSource/");
+                this.validSourcePath = Path.Combine(
+                    this.testPath,
+                    "ValidPackageSource/");
+
+                Directory.CreateDirectory(testPath);
+                Directory.CreateDirectory(emptySourcePath);
+                Directory.CreateDirectory(validSourcePath);
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                Directory.Delete(this.testPath, true);
+            }
+
+            protected override IPackageProvider CreateProvider()
+            {
+                return new FileSystemPackageProvider();
+            }
+
+            protected override string GetInvalidSourcePath()
+            {
+                return emptySourcePath;
+            }
+
+            protected override string GetValidSourcePath()
+            {
+                return validSourcePath;
+            }
+        }
+    }
+}
