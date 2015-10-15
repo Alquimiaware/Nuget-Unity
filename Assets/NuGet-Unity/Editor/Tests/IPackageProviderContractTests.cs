81a42f198eb38ec78ee98978464492a8d663aa95
Rafael Muñoz <raudo18@gmail.com>
2015-10-15 17:02:49 +0200

    Added contract tests for IPackageProvider interface

---
+++ Assets/NuGet-Unity/Editor/Tests/IPackageProviderContractTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Alquimiaware.NuGetUnity.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public abstract class IPackageProviderContractTests
+    {
+        [Test]
+        public void IsPackageSource_NullOrEmptyPath_ReturnsFalse()
+        {
+            var sut = CreateProvider();
+            Assert.IsFalse(sut.IsPackageSource(null));
+            Assert.IsFalse(sut.IsPackageSource(string.Empty));
+        }
+
+        [Test]
+        public void GetAll_IsNotPackageSource_Fails()
+        {
+            var sut = CreateProvider();
+            string invalidSourcePath = GetInvalidSourcePath();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => sut.GetAll(invalidSourcePath));
+        }
+
+        [Test]
+        public void GetAll_IsValidSource_ReturnsNonEmptyList()
+        {
+            var sut = CreateProvider();
+            var validPath = GetValidSourcePath();
+            var packages = sut.GetAll(validPath);
+            Assert.IsNotNull(packages);
+            CollectionAssert.IsNotEmpty(packages);
+            CollectionAssert.AllItemsAreNotNull(packages);
+        }
+
+        [Test]
+        public void GetInvalidSourcePath_ByDefinition_IsAnInvalidPath()
+        {
+            var sut = CreateProvider();
+            string invalidPath = GetInvalidSourcePath();
+            Assert.IsFalse(sut.IsPackageSource(invalidPath));
+        }
+
+        [Test]
+        public void GetValidSourcePath_ByDefinition_IsAValidPath()
+        {
+            var sut = CreateProvider();
+            string validSource = GetValidSourcePath();
+            Assert.IsTrue(sut.IsPackageSource(validSource));
+        }
+
+        protected abstract string GetInvalidSourcePath();
+        protected abstract string GetValidSourcePath();
+        protected abstract IPackageProvider CreateProvider();
+    }
+
+}
