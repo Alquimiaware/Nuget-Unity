734ffa46a0c9731359cf24420e1d98a71b2687a4
Rafael Muñoz <raudo18@gmail.com>
2015-11-03 00:28:16 +0100

    Added target preferences constant with fallback target when no target found for the two most common unity profiles. Subset and Full

---
+++ Assets/NuGet-Unity/Editor/TargetPreferences.cs
@@ -0,0 +1,38 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using System.Collections.Generic;
+
+    public class TargetPreferences
+    {
+        public readonly static TargetPreferences DotNetFull = new TargetPreferences(
+            Net35Target,
+            UnityFullTarget, UnitySubsetTarget);
+
+        public readonly static TargetPreferences DotNetSubset = new TargetPreferences(
+            Net35Target,
+            UnitySubsetTarget);
+
+        // Target official names
+        private const string UnityFullTarget = "net35-Unity Full v3.5";
+        private const string UnitySubsetTarget = "net35-Unity Subset v3.5";
+        private const string Net35Target = "net35";
+
+        private TargetPreferences(
+            string fallbackTarget,
+            params string[] decreasingPriorityTargets)
+        {
+            this.FallbackTarget = fallbackTarget;
+            this.DecreasingPriorityTargets = decreasingPriorityTargets;
+        }
+
+        public IEnumerable<string> DecreasingPriorityTargets
+        {
+            get; private set;
+        }
+
+        public string FallbackTarget
+        {
+            get; private set;
+        }
+    }
+}
