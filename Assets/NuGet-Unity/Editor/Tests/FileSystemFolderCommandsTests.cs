f06286137cd348e5790e35aac6818510ed9e38ca
Rafael Muñoz <raudo18@gmail.com>
2015-11-12 17:33:44 +0100

    Created contract tests for IFolderCommands

---
+++ Assets/NuGet-Unity/Editor/Tests/FileSystemFolderCommandsTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Alquimiaware.NuGetUnity.Tests
+{
+    using UnityEngine;
+    using System.Collections;
+    using NUnit.Framework;
+    using System.IO;
+    using System;
+
+    [TestFixture]
+    public class FileSystemFolderCommandsTests
+    {
+        [Test]
+        public void CanCreate()
+        {
+            var sut = new FileSystemFolderCommands();
+        }
+
+        public class Contracts : IFolderCommandsContractTests
+        {
+            protected override string GetExistingFolderPath()
+            {
+                var workingDir = GetTestWorkingDir();
+                Directory.CreateDirectory(workingDir);
+
+                return workingDir;
+            }
+
+            protected override string GetNonExistingFolderPath()
+            {
+                return Path.Combine(
+                    GetTestWorkingDir(), "FooBar123");
+            }
+
+            protected override IFolderCommands GetFolderCommands()
+            {
+                return new FileSystemFolderCommands();
+            }
+
+        }
+
+        private static string GetTestWorkingDir()
+        {
+            return Path.Combine(
+                Application.dataPath,
+                "../Library/IFolderCommandsContractTests/");
+        }
+    }
+
+}
---
+++ Assets/NuGet-Unity/Editor/Tests/IFolderCommandsContractTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Alquimiaware.NuGetUnity.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture, Category("Integration")]
+    public abstract class IFolderCommandsContractTests
+    {
+        [Test]
+        public void Exists_OfExistingFolder_IsTrue()
+        {
+            var sut = GetFolderCommands();
+            var existingFolderPath = GetExistingFolderPath();
+
+            bool exists = sut.Exists(existingFolderPath);
+            Assert.IsTrue(exists);
+        }
+
+        [Test]
+        public void Exists_OfNonExistingFolder_IsFalse()
+        {
+            var sut = GetFolderCommands();
+            var existingFolderPath = GetExistingFolderPath();
+
+            bool exists = sut.Exists(existingFolderPath);
+            Assert.IsTrue(exists);
+        }
+
+        [Test]
+        public void GetExistingFolderPath_ByDefinition_ReturnsExistingFolderPath()
+        {
+            var path = GetExistingFolderPath();
+            var sut = GetFolderCommands();
+            Assert.IsTrue(sut.Exists(path));
+        }
+
+        [Test]
+        public void GetNonExistingFolderPath_ByDefinition_RetunrsNonExistingPath()
+        {
+            var path = GetNonExistingFolderPath();
+            var sut = GetFolderCommands();
+            Assert.IsFalse(sut.Exists(path));
+        }
+
+        protected abstract IFolderCommands GetFolderCommands();
+        protected abstract string GetExistingFolderPath();
+        protected abstract string GetNonExistingFolderPath();
+    }
+}
