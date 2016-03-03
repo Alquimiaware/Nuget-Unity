c2ce67cf61f105ac5ddbe27018c37abeaf263849
Rafael Muñoz <raudo18@gmail.com>
2016-03-03 21:50:15 +0100

    Added example of usage of editor progress bar

---
+++ Assets/NuGet-Unity/Editor/ProgressBarWindows.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ProgressBarWindows : EditorWindow
+{
+    private bool shouldShowProgressBar;
+
+    [MenuItem("Examples/Cancelable Progress Bar Usage")]
+    static void Init()
+    {
+        var window = GetWindow<ProgressBarWindows>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Display bar"))
+            this.shouldShowProgressBar = true;
+
+        if (this.shouldShowProgressBar)
+        {
+            if (EditorUtility.DisplayCancelableProgressBar(
+                "Simple Progress Bar",
+                "Shows some random info",
+                0.99f))
+            {
+                Debug.Log("Progress bar canceled by the user");
+                this.shouldShowProgressBar = false;
+                EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+
+    private void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+}
