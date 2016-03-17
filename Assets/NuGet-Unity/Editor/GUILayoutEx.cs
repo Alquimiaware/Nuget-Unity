3631ed31727e0350889f1de62af37eabf119635b
Rafael Muñoz <raudo18@gmail.com>
2016-03-17 20:14:57 +0100

    Added GUILayoutEx

---
+++ Assets/NuGet-Unity/Editor/GUILayoutEx.cs
@@ -0,0 +1,68 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using UnityEngine;
+    using System;
+    using UnityEditor;
+
+    public class GUILayoutEx
+    {
+        public static IDisposable Vertical()
+        {
+            EditorGUILayout.BeginVertical();
+            return new ActionOnDispose(() => EditorGUILayout.EndVertical());
+        }
+
+        public static IDisposable Vertical(params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginVertical(options);
+            return new ActionOnDispose(() => EditorGUILayout.EndVertical());
+        }
+
+        public static IDisposable Vertical(
+            GUIStyle style,
+            params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginVertical(style, options);
+            return new ActionOnDispose(() => EditorGUILayout.EndVertical());
+        }
+
+        public static IDisposable Horizontal()
+        {
+            EditorGUILayout.BeginHorizontal();
+            return new ActionOnDispose(() => EditorGUILayout.EndHorizontal());
+        }
+
+        public static IDisposable Horizontal(params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginHorizontal(options);
+            return new ActionOnDispose(() => EditorGUILayout.EndHorizontal());
+        }
+
+        public static IDisposable Horizontal(
+            GUIStyle style,
+            params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginHorizontal(style, options);
+            return new ActionOnDispose(() => EditorGUILayout.EndHorizontal());
+        }
+
+        private class ActionOnDispose : IDisposable
+        {
+            private Action action;
+            private bool isDisposed;
+
+            public ActionOnDispose(Action action)
+            {
+                this.action = action;
+            }
+
+            public void Dispose()
+            {
+                if (this.isDisposed) return;
+
+                this.isDisposed = true;
+                this.action();
+            }
+        }
+    }
+}
