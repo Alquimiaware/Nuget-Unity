a8bc9f7798e42be8a37880e184a544a167287f4c
Rafael Muñoz <raudo18@gmail.com>
2016-03-17 22:40:15 +0100

    Create KeyEvent to Read Editor Keyboard events

---
+++ Assets/NuGet-Unity/Editor/KeyEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Alquimiaware
+{
+    using UnityEngine;
+
+    public class KeyEvent
+    {
+        public static bool JustReleased(KeyCode keycode)
+        {
+            // Key events should only be triggered during their event type
+            return Event.current.type == EventType.KeyUp
+                && Event.current.keyCode == keycode;
+        }
+    }
+}
