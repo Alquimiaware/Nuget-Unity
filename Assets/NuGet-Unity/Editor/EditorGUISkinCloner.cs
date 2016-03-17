23038e45084ce4122209fce5015ab32b170b5c59
Rafael Muñoz <raudo18@gmail.com>
2016-03-17 23:45:51 +0100

    Added Helper Assert Creator that will cleate a clone of EditorGUISkin

---
+++ Assets/NuGet-Unity/Editor/EditorGUISkinCloner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Credits to dodo:
+// http://forum.unity3d.com/threads/is-there-a-unity-editor-gui-skin-template-floating-around-somewhere.170632/
+public class DummyEditor : EditorWindow
+{
+    [MenuItem("Assets/Clone Editor Skin")]
+    static public void CloneEditorSkin()
+    {
+        GUISkin skin = ScriptableObject.Instantiate(
+            EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector)) as GUISkin;
+        AssetDatabase.CreateAsset(skin, "Assets/EditorSkinClone.guiskin");
+        Selection.activeObject = skin;
+    }
+}
