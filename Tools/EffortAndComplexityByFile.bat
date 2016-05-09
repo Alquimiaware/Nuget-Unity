4e9ae5eb663eeb0f34afe346cf33670db54007a1
Rafael Muñoz <raudo18@gmail.com>
2016-05-09 13:05:03 +0200

    Added a batch to generate a csv merging complexity and effort by file

---
+++ Tools/EffortAndComplexityByFile.bat
@@ -0,0 +1,16 @@
+call "%~dp0SetupTools.bat"
+
+:: get git log
+set logName=RepoLog.log
+git log --pretty=format:"[%%h] %%an %%ad %%s" --date=short --numstat > %logName% 
+
+:: Perform Summary Analysis
+set effortFilename=RevisionsByFile.csv
+call maat -c git -l %logName% -a revisions > %effortFilename%
+
+:: Complexity by file
+set complexityFilename=ComplexityByFile.csv
+cloc .\ --by-file --csv --quiet --report-file=%complexityFilename%
+
+:: Combine Effort And Complexity
+python .\Tools\merge_comp_freqs.py %effortFilename% %complexityFilename% > EffortAndComplexityByFile.csv
