94b731c493ae5618caf0054dd1549e51011f0cb5
Rafael Muñoz <raudo18@gmail.com>
2016-05-09 12:15:08 +0200

    Removed unused filed

--- Tools/Foo.bat
+++
@@ -1,7 +0,0 @@
-call SetToolsPath.bat
-echo paths to bat 
-echo fullpath %0
-echo drive %~d0
-echo folder %~dp0
-echo name %~n0
-pause
--- Tools/SetToolsPath.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo paths to bat 
-echo fullpath %0
-echo drive %~d0
-echo folder %~dp0
-echo name %~n0
