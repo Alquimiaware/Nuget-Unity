5a5a8faa65180fca5590ea3079ecdad47a895960
Rafael Muñoz <raudo18@gmail.com>
2016-05-09 13:06:21 +0200

    Updating setup tools to include python27 and cloc

--- Tools/SetupTools.bat
+++ Tools/SetupTools.bat
@@ -1,6 +1,13 @@
 :: Setting Up All Required Tools
+:: Project Root
+set ProjectRoot=%~dp0..\
+cd %ProjectRoot%
 :: Git
 set GIT_PAGER=cat
 set PATH=%PATH%;%PROGRAMFILES(X86)%\Git\cmd
 :: Code Maat
 set PATH=%PATH%;%PROGRAMFILES(X86)%\winmaat0.8.5
+:: Cloc
+set PATH=%PATH%;%PROGRAMFILES(X86)%\cloc
+:: Python27
+set PATH=%PATH%;C:\Python27
