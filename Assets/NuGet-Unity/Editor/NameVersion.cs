6684d816a1c467ee3a1a3f5f1c382540b4b9881a
Rafael Muñoz <raudo18@gmail.com>
2016-06-02 18:47:23 +0200

    Implemented NameVersion Parser to pass the good citizen tests raw names

--- Assets/NuGet-Unity/Editor/NameVersion.cs
+++ Assets/NuGet-Unity/Editor/NameVersion.cs
@@ -1,5 +1,7 @@
 ﻿namespace Alquimiaware.NuGetUnity
 {
+    using System.Text.RegularExpressions;
+
     public class NameVersion
     {
         public string Name { get; private set; }
@@ -7,15 +9,34 @@
 
         public static NameVersion Parse(string versionedPackageName)
         {
-            var terms = versionedPackageName.Split(' ');
-            var name = terms[0];
-            var version = terms[1];
+            string input = versionedPackageName;
 
-            return new NameVersion()
+            string pattern = @"^(?<name>([a-zA-Z]+\w*\.?)+)(\s+|\.)(?<version>(\d+\.?){2,4}(-[\w-]+)?)";
+
+            var match = Regex.Match(input, pattern);
+
+            UnityEngine.Debug.Log("Input: " + input);
+
+            if (match.Success)
             {
-                Name = name,
-                Version = version
-            };
+                
+                GroupCollection groups = match.Groups;
+                ////UnityEngine.Debug.LogFormat("{0} groups captured in <{1}>", groups.Count, match.Value);
+                ////for (int i = 0; i < groups.Count; i++)
+                ////{
+                ////    UnityEngine.Debug.LogFormat("Group {0}: <{1}>", i, groups[i]);
+                ////}
+                UnityEngine.Debug.LogFormat("Name: <{0}>",groups["name"]);
+                UnityEngine.Debug.LogFormat("Version: <{0}>",groups["version"]);
+                return new NameVersion()
+                {
+                    Name = groups["name"].Value,
+                    Version = groups["version"].Value
+                };
+            }
+
+
+            throw new System.ArgumentOutOfRangeException();
         }
     }
 }
--- Assets/NuGet-Unity/Editor/Tests/NameVersionTests.cs
+++ Assets/NuGet-Unity/Editor/Tests/NameVersionTests.cs
@@ -6,7 +6,7 @@
     public class NameVersionTests
     {
         [Test]
-        public void NameBatch()
+        public void NameParsing()
         {
             // Point Sepparator
             AssertName("SocialSense.1.2", "SocialSense");
@@ -19,7 +19,7 @@
             AssertName("Http.Features.1.0.0-rc1-final", "Http.Features");
             AssertName("Mvc.4.0.20710.0", "Mvc");
 
-            // Space Sepparator
+            ////// Space Sepparator
             AssertName("AppNet.NET 1.8", "AppNet.NET");
             AssertName("Acr.Settings 5.1.0", "Acr.Settings");
             AssertName("Facebook 7.0.10.3400", "Facebook");
@@ -28,7 +28,7 @@
         }
 
         [Test]
-        public void VersionBatch()
+        public void VersionParsing()
         {
             // Point Sepparator
             AssertVersion("SocialSense.1.2", "1.2");
