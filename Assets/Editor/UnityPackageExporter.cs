6d366693cec0c17f4c183a3e46a0c7b3ad2b1ab3
Rafael Muñoz <raudo18@gmail.com>
2017-08-07 15:01:16 +0200

    Refactor to make exporting as unity package easier

---
+++ Assets/Editor/UnityPackageExporter.cs
@@ -0,0 +1,14 @@
+﻿namespace Alquimiaware.NuGetUnity
+{
+    using UnityEditor;
+
+    public class UnityPackageExporter
+    {
+        [MenuItem("Export/Nuget Unity")]
+        public static void ExportPackage()
+        {
+            AssetDatabase.ExportPackage("Assets/NuGet-Unity", "NuGetUnity.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+        }
+
+    }
+}
