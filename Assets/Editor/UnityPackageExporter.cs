eb91e2cb85db8bf322d6d9fbe31ed64222e91c7f
Rafael Muñoz <raudo18@gmail.com>
2017-08-11 12:01:09 +0200

    Exporter retains last export path

--- Assets/Editor/UnityPackageExporter.cs
+++ Assets/Editor/UnityPackageExporter.cs
@@ -1,13 +1,28 @@
 ﻿namespace Alquimiaware.NuGetUnity
 {
+    using System.IO;
     using UnityEditor;
 
     public class UnityPackageExporter
     {
+        private const string LastOutputFolderKey = "NuGet_UnityPackageExporter_LastOutputFolder";
+
         [MenuItem("Export/Nuget Unity")]
         public static void ExportPackage()
         {
-            AssetDatabase.ExportPackage("Assets/NuGet-Unity", "NuGetUnity.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+            string path = EditorUtility.SaveFilePanel(
+                "Select package destination",
+                EditorPrefs.GetString(LastOutputFolderKey, string.Empty),
+                "NuGetUnity",
+                "unitypackage");
+
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            string directory = Path.GetDirectoryName(path);
+            EditorPrefs.SetString(LastOutputFolderKey, directory);
+
+            AssetDatabase.ExportPackage("Assets/NuGet-Unity", path, ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
         }
 
     }
